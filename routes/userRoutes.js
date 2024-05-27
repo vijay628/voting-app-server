4e767a5b430871caed746396f6620ea8030e9c5f
vijay628 <durgvijaymaurya21@gmail.com>
2024-05-28 03:24:48 +0530

    new

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/UserModel');
 const { jwtAuthMiddleware, generateToken } = require('../jwt');
+const axios = require('axios');
 
 
 // validate email
