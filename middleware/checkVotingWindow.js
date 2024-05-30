e677a47655cc833557c672998eefe3b20e7d1b19
vijay628 <durgvijaymaurya21@gmail.com>
2024-05-30 19:55:35 +0530

    time limit issue fix

--- middleware/checkVotingWindow.js
+++ middleware/checkVotingWindow.js
@@ -1,21 +1,54 @@
 const VotingWindow = require('../models/VotingWindow');
 
+// Function to format date and time in UTC with AM/PM
+function formatDateTimeUTC(dateString) {
+    const date = new Date(dateString);
+    const year = date.getUTCFullYear();
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); 
+    const day = date.getUTCDate().toString().padStart(2, '0');
+    let hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+    const hoursStr = hours.toString().padStart(2, '0');
+
+    return `${day}-${month}-${year} ${hoursStr}:${minutes} ${ampm} UTC`;
+}
+
 const checkVotingWindow = async (req, res, next) => {
     try {
         const votingWindow = await VotingWindow.findOne();
         if (!votingWindow) {
-            return res.status(403).json({ message: 'Voting window not set' });
+            return res.status(403).json({ message: 'Voting Not Started or Ended. Thank you!' });
         }
 
         const now = new Date();
-        if (now < votingWindow.startDate || now > votingWindow.endDate) {
-            return res.status(403).json({ message: 'Voting is only allowed within the specified time window.' });
+        const nowUTC = new Date(now.getTime() + (now.getTimezoneOffset() * 60000) - 1000*60*60 ); // Convert current date to UTC
+        const startDate = new Date(votingWindow.startDate);
+        const endDate = new Date(votingWindow.endDate);
+
+        const startDateUTC = new Date(startDate.getTime() + (2*startDate.getTimezoneOffset() * 60000) - 1000*60*60);
+        const endDateUTC = new Date(endDate.getTime() + (2*endDate.getTimezoneOffset() * 60000) - 1000*60*60);
+        // console.log("Now:", nowUTC);
+        // console.log("Start Date:", startDateUTC);
+        // console.log("End Date:", endDateUTC);
+        // console.log(now.getTime(),now.getTimezoneOffset());
+        // console.log(startDate.getTime(), endDate.getTime());
+
+
+        if (nowUTC >= startDateUTC && nowUTC <= endDateUTC) {
+            return next(); // Current date is within the voting window
+        } else {
+            return res.status(403).json({
+                message: `Voting is scheduled between ${formatDateTimeUTC(votingWindow.startDate)} and ${formatDateTimeUTC(votingWindow.endDate)}.`
+            });
         }
-        next();
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
 
-module.exports = {checkVotingWindow};
+module.exports = { checkVotingWindow };
